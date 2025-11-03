7db92503a64f9985911d1283fafb464b584e0cf0
Marcelo dos Reis <marcelohrpaulo13@gmail.com>
2025-11-02 23:10:36 -0300

    feat: add assets

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,3 @@
-import './style.css'
-import viteLogo from '/vite.svg'
+// import { engine } from "./core/engine";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+// engine.loadSprite("spritesheet");
